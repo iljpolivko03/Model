73534a82557fca45e7fe1d1d64b65c39eb19a6cb
iljpolivko03 <alexanderpolivko73@gmail.com>
2018-12-01 09:56:04 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,14 +3,14 @@ require 'connection.php';
 $app=new \atk4\ui\App('Admin');
 $app->initLayout('Centered');
 
-$crud=$app->add('Crud');
+$crud=$app->add('CRUD');
 $crud->setModel(new Clients($db));
 
-$crud1=$app->add('Crud');
+$crud1=$app->add('CRUD');
 $crud1->setModel(new Friends($db));
 
-$crud2=$app->add('Crud');
+$crud2=$app->add('CRUD');
 $crud2->setModel(new Loan($db));
 
-$crud3=$app->add('Crud');
+$crud3=$app->add('CRUD');
 $crud3->setModel(new Vozvrat($db));
